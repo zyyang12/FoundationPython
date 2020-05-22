0d7fbcfa7fe98c92eb3746255a72d6c22abd8d87
zyyang12 <52126653+zyyang12@users.noreply.github.com>
2020-05-22 14:53:15 +0800

    Update obj2dict.py
    修复，词典key值错位的bug

--- obj2dict.py
+++ obj2dict.py
@@ -1,21 +1,24 @@
-def list2dict(datalist, keyname):
+#本模块实现python对象object递归转dict
+def list2dict(datalist, keyname, key):
+    keyname = keyname + "." + key
     for i in range(len(datalist)):
-        keyname = keyname + "." + str(i)
+        keynamelist = keyname + "." + str(i)
         if isinstance(datalist[i], dict):
-            obj2dict(datalist[i], keyname)
+            obj2dict(datalist[i], keynamelist)
         elif isinstance(datalist[i], list):
-            list2dict(datalist[i], keyname)
+            list2dict(datalist[i], keynamelist2, "")
         else:
-            result_dict[keyname] = datalist[i]
-#递归实现object转dict
+            result_dict[keynamelist] = datalist[i]
+#此功能模块的入口函数
+#result_dict全局变量，存放转换后的词典结果
 def obj2dict(data_obj, keyname):
     isinstance(data_obj, dict)
     for key in data_obj.keys():
         if isinstance(data_obj[key], dict):
-            keyname = keyname + "." + key
-            obj2dict(data_obj[key],keyname)
+            keynamedict = keyname + "." + key
+            obj2dict(data_obj[key],keynamedict)
         elif isinstance(data_obj[key], list):
-            list2dict(data_obj[key], keyname)
+            list2dict(data_obj[key], keyname, key)
         else:
-            keyname = keyname + "." + key
-            result_dict[keyname] = data_obj[key]
+            keynamestr = keyname + "." + key
+            result_dict[keynamestr] = data_obj[key]
